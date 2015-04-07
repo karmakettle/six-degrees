9bf092611b07ca75dfe88f7d78fa239e118855d9
karmakettle <karmakettle@gmail.com>
2015-04-07 10:16:49 -0500

    added listener to dropdown menu, confirmConnection function

--- app.js
+++ app.js
@@ -51,28 +51,86 @@ var app = {
   },
 
   displayMovieSearch: function(node, results){
-    var resultHtml = "";
+    var dropdown = $('<ul class="active"></ul>');
     for ( var i = 0; i < results.length; i++ ) {
       var movie = results[i];
-      resultHtml += "<li>" + movie.title + ", " + movie.release_date + "</li>";
+      dropdown.append('<li data-id="' + movie.id + '">' + movie.title + ', ' + movie.release_date + '</li>')
     }
-    node.after('<div class="active">' + resultHtml + '</div>');
+    node.after(dropdown);
+    $('li').on('click', app.selectMovie);
   },
 
   displayActorSearch: function(node, results){
-    var resultHtml = "";
+    var dropdown = $('<ul class="active"></ul>');
     for ( var i = 0; i < results.length; i++ ) {
       var actor = results[i];
-      resultHtml += "<li>" + actor.name + "</li>";
+      dropdown.append('<li data-id="' + actor.id + '">' + actor.name + '</li>')
     }
-    node.after('<div class="active">' + resultHtml + '</div>');
+    node.after(dropdown);
+    $('li').on('click', app.selectActor);
+  },
+
+  selectActor: function(){
+    var id = $(this).data('id');
+    app.compareActor = id;
+    // if there's already a compareMovie in existence, compare, remove the display box
+    if ( app.compareMovie ) {
+      // if the actor's name is in the movie's credits, 'solidify' the fields and remove disabled from the next ones
+      if ( app.confirmConnection() ) {
+        // if goalActor, winning sequence!
+        // else
+          // startActor = compareActor;
+          // compareActor = undefined;
+          // compareMovie = undefined;
+          // remove .active class to remove dropdown
+          // remove inputs and add h2s with names and photos
+          // set background / highlight
+      } else {
+        // there's not a match, display error
+        console.log("These actors are not in a movie together.")
+      }
+    }
+
+    $('.active').remove();
+  },
+
+  selectMovie: function(){
+    var id = $(this).data('id');
+    app.compareMovie = id;
+    // if there's already a compareActor in existence, compare
+    if ( app.compareActor ) app.confirmConnection();
+    // remove the display box
+    $('.active').remove();
+  },
+
+  confirmConnection: function(){
+    // get movie credits from compareMovie
+    $.ajax({
+      url: "http://api.themoviedb.org/3/movie/" + app.compareMovie + "/credits?api_key=b94d3520d22303948e683ac7f88387c7",
+      accept: "application/json"
+    }).done(function(results){
+      var cast = results.cast.map(function(person){
+        return person.name;
+      });
+
+      // if both actors' names are in the movie's credits
+        //'solidify' the fields and remove disabled from the next ones
+          // if goalActor, winning sequence!
+          // else
+            // startActor = compareActor;
+            // compareActor = undefined;
+            // compareMovie = undefined;
+            // remove .active class to remove dropdown
+            // remove inputs and add h2s with names and photos
+            // set background / highlight
+    });
   }
 
 };
 
 $(document).ready(function(){
   $('input').keypress(function(e){
-    if (e.keyCode === 13) {
+    if ( e.keyCode === 13 ) {
       // get info:  whether it's actor or movie
       var guess = $(this).val();
       // if this parent's class is a movie

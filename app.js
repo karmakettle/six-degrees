5d2d209d2e0aa13d123cfe628659f1f85f69e05e
karmakettle <karmakettle@gmail.com>
2015-04-06 23:48:08 -0500

    improved random actor generator--limited to most popular

--- app.js
+++ app.js
@@ -2,30 +2,39 @@
 var app = {
   init: function(){
     app.actors = [];
-    app.getActor(app.generateRandom(), true);
-    app.getActor(app.generateRandom());
-    // app.displayActorImage($('#start'), app.actors[0]);
+    app.generateRandom();
   },
 
   generateRandom: function(){
-    return Math.floor(Math.random() * 1000);
+    // if app is initializing, set a goal actor; else, add to actors array
+    $.ajax({
+      url: "http://api.themoviedb.org/3/person/popular?api_key=b94d3520d22303948e683ac7f88387c7",
+      accept: "application/json"
+    }).done(function(personList){
+      var startActor = personList.results[Math.floor(Math.random() * personList.results.length)];
+      var goalActor = personList.results[Math.floor(Math.random() * personList.results.length)];
+      app.actors.push(startActor);
+      app.displayActorInfo($('#start'), startActor);
+      app.goal = goalActor;
+      app.displayActorInfo($('.goal'), goalActor);
+    });
   },
 
   getActor: function(id, isInit){
-    // if app is initializing, set a goal actor; else, add to actors array
     $.ajax({
       url: "http://api.themoviedb.org/3/person/" + id + "?api_key=b94d3520d22303948e683ac7f88387c7",
       accept: "application/json"
     }).done(function(data){
-      if (isInit) { 
-        app.goal = data;
-        app.displayActorInfo($('.goal'), data);
-
-      } else {
-        app.actors.push(data);
-        app.displayActorInfo($('#start'), data);
-      }
     })
+  },
+
+  movieSearch: function(node, guess){
+    $.ajax({
+      url: "http://api.themoviedb.org/3/search/movie?api_key=b94d3520d22303948e683ac7f88387c7&query=" + guess,
+      accept: "application/json"
+    }).done(function(results){
+      app.displayMovieSearch(node, results.results);
+    });
   },
 
   displayActorInfo: function(node, actor){
@@ -38,6 +47,16 @@ var app = {
             });
     node.children('input').remove();
     node.append('<h2>' + actor.name + '</h2>');
+  },
+
+  displayMovieSearch: function(node, results){
+    console.log(results);
+    var resultHtml = "";
+    for ( var i = 0; i < results.length; i++ ) {
+      var movie = results[i];
+      resultHtml += "<li>" + movie.title + ", " + movie.release_date + "</li>";
+    }
+    node.after('<div class="active">' + resultHtml + '</div>');
   }
 
 };
@@ -46,6 +65,15 @@ $(document).ready(function(){
   $('input').keypress(function(e){
     if (e.keyCode === 13) {
       // get info:  whether it's actor or movie
+      var guess = $(this).val();
+      // if this parent's class is a movie
+      if ( $(this).parent().hasClass('movie') ) {
+        console.log('hooray movie!');
+        // get list of possible matches by searching tmdb
+        app.movieSearch($(this), guess);
+        // display list of options underneath input
+      }
+      // if it's a movie, check last actor?
     }
   });
 });
